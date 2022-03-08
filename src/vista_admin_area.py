ed52c9a3df8835a2d8d45eab04468a1b07e52f7e
ArthourVaSa <arjavasaes21@gmail.com>
2022-03-08 17:34:02 -0500

    cambios en las vistas y añadiendo nuevas vistas

--- src/vista_admin_area.py
+++ src/vista_admin_area.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 import sys
 
 from PyQt5 import uic
--- src/vista_principal.py
+++ src/vista_principal.py
@@ -17,13 +17,11 @@ class VentanaPrincipal(QMainWindow):
         uic.loadUi("vistas/VistaMain.ui", self)
 
         self.nombre_carpeta_tipo_doc = db.session.query(TipoDoc.tipo_doc).filter_by(tipo_doc="factura").first()
-        print(self.nombre_carpeta_tipo_doc)
 
         # os.makedirs("D:/Arthour/Trabajos/Nexuzz/Archivos/documentos/"+self.nombre_carpeta_tipo_doc[0])
 
         self.modelo = QFileSystemModel(self)
         self.modelo.setRootPath("D:/Arthour/Trabajos/Nexuzz/Archivos/documentos/")
-        print(self.modelo)
 
         self.treeViewBuscador.setModel(self.modelo)
         self.treeViewBuscador.setRootIndex(self.modelo.index("D:/Arthour/Trabajos/Nexuzz/Archivos/documentos/"))
@@ -32,6 +30,7 @@ class VentanaPrincipal(QMainWindow):
         self.nombre_i_b = db.session.query(IndiceBusqueda.nombre_archivo).filter_by(nombre_archivo="CV.pdf").first()
         self.lista_indices.append("nombre")
         self.nombre_i_b_mapa = db.session.query(IndiceBusqueda.indice_busqueda).filter(TipoDoc.id_indice_busqueda == IndiceBusqueda.id).first()
+        print(self.nombre_i_b_mapa)
         mapa_indices = ast.literal_eval(self.nombre_i_b_mapa[0])
         self.counter = 1
         self.row = 0
@@ -40,14 +39,11 @@ class VentanaPrincipal(QMainWindow):
             self.lista_indices.append(k)
         self.tableWidgetLlenado.setColumnCount(self.counter)
         self.tableWidgetLlenado.setHorizontalHeaderLabels(self.lista_indices)
-        for value in mapa_indices.values():
-            self.tableWidgetLlenado.insertRow(self.row)
-            nombre = QTableWidgetItem(str(self.nombre_i_b[0]))
-            # ruc = QTableWidgetItem(value))
-            # fecha
-            # monto
-            # sede
-            self.tableWidgetLlenado.setItem(self.row,0,nombre)
+        self.tableWidgetLlenado.insertRow(self.row)
+        nombre = QTableWidgetItem(str(self.nombre_i_b[0]))
+        ruc = QTableWidgetItem(str(mapa_indices['ruc']))
+        self.tableWidgetLlenado.setItem(self.row,0,nombre)
+        self.tableWidgetLlenado.setItem(self.row,1,ruc)
 
 
         self.actionCargar_Archivos.triggered.connect(self.abrir_pdf)
