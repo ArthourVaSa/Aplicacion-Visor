30680c078aea8cd0286c92a50423a78350760289
ArthourVaSa <arjavasaes21@gmail.com>
2022-03-29 18:08:22 -0500

    Finalizada la 2da parte, avanzando con la 3era

--- src/vista_admin_archivos_usuarios.py
+++ src/vista_admin_archivos_usuarios.py
@@ -11,6 +11,8 @@ class VistaAdminArchivosUser(QDialog):
         QDialog.__init__(self)
         uic.loadUi("src/vistas/VistaAdminArchivosUsuarios.ui",self)
 
+        self.lista_areas = []
+        self.lista_usuario = []
         self.texto = texto
 
         self.setWindowTitle("Administrador")
@@ -19,4 +21,54 @@ class VistaAdminArchivosUser(QDialog):
 
         #parte de la relación
         self.id_empresa = db.session.query(Empresa.id).filter(Empresa.nombre_empresa == self.texto).first()
-        self.tipo_doc = db.session.query(TipoDoc.tipo_doc).filter(Empresa.id == Area.id_empresa, )
+        self.areas = db.session.query(Area).filter(Area.id_empresa == self.id_empresa[0]).all()
+        for datos in self.areas:
+            self.lista_areas.append(str(datos))
+        
+        self.comboBox_area.addItems(self.lista_areas)
+        self.comboBox_area.activated[str].connect(self.onChanged)
+
+        self.usuarios = db.session.query(User).filter(User.empresa_id == self.id_empresa[0]).all()
+        for user in self.usuarios:
+            self.lista_usuario.append(str(user))
+
+        self.comboBox_user.addItems(self.lista_usuario)
+
+        #botones
+        self.pushButton_guardar.clicked.connect(self.guardar_relacion)
+
+    def onChanged(self):
+        self.comboBox_tipo_doc.clear()
+        area = self.comboBox_area.currentText()
+        
+        relacion = db.session.query(TipoDoc.tipo_doc).filter(TipoDoc.area.any(Area.nombre_area == str(area))).all()
+        if(len(relacion) == 0):
+            mensaje = QMessageBox()
+            mensaje.setText("ERROR")
+            mensaje.setInformativeText("No existe este tipo documental en el área.")
+            mensaje.setWindowTitle("Error")
+            mensaje.exec_()
+        else:
+            self.comboBox_tipo_doc.addItems(relacion[0])
+
+    def guardar_relacion(self):
+        try:
+            tipo_d = self.comboBox_tipo_doc.currentText()
+            usuario = self.comboBox_user.currentText()
+
+            q_tipo_d = db.session.query(TipoDoc).filter(TipoDoc.tipo_doc == str(tipo_d)).all()
+            q_usuario = db.session.query(User).filter(User.username == str(usuario), User.empresa_id == self.id_empresa[0]).all()
+
+            q_usuario[0].tipo_documental += [q_tipo_d[0]]
+            db.session.commit()
+
+            mensaje.setText("Tarea Realizada")
+            mensaje.setInformativeText("La tarea ha sido cumplida. Se han guardado las relaciones de manera satisfactoriamente")
+            mensaje.setWindowTitle("Tarea completa")
+            mensaje.exec_()
+        except:
+            mensaje = QMessageBox()
+            mensaje.setText("ERROR")
+            mensaje.setInformativeText("Ha ocurrido un error al momento de realizar la tarea")
+            mensaje.setWindowTitle("Error")
+            mensaje.exec_()
--- src/vista_principal.py
+++ src/vista_principal.py
@@ -1,20 +1,51 @@
+import os
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from src.vista_admin_inicio import VistaAdminInicio
+from src.vista_super_inicio import VistaSuperInicio
+
+from src.database import db
+from src.database.modelos import *
+
+import time
 
 class VentanaPrincipal(QMainWindow):
-    def __init__(self):
+    def __init__(self, empresa, nombre_usuario):
         QMainWindow.__init__(self)
         uic.loadUi("src/vistas/VistaMain.ui", self)
 
-        self.modelo = QFileSystemModel()
-        self.modelo.setRootPath('')
+        self.empresa = empresa
+        self.nombre_usuario = nombre_usuario
 
-        self.treeViewBuscador.setModel(self.modelo)
-
+        #parte actions
+        rol = db.session.query(User.rol_id).filter(User.username == self.nombre_usuario).first()
+        nombre_rol = db.session.query(Rol.rol_nombre).filter(Rol.id == rol[0]).first()
+            
+        if(nombre_rol[0] == "SuperUsuario"):
+            self.actionAdministrador.setEnabled(False)
+        elif (nombre_rol[0] == "Administrador"):
+            self.actionSuperUsuario.setEnabled(False)
+        elif (nombre_rol[0] == "Usuario"):
+            self.actionAdministrador.setEnabled(False)
+            self.actionSuperUsuario.setEnabled(False)
+            
         self.actionCargar_Archivos.triggered.connect(self.abrir_pdf)
+        self.actionAdministrador.triggered.connect(self.go_to_admin_area)
+        self.actionSuperUsuario.triggered.connect(self.go_to_super_inicio)
+
+        #parte del treeview
+        path_local = os.getcwd()
+        path_ruta_local = path_local.replace("\\","/")
+        self.treeViewBuscador.doubleClicked.connect(self.jalar_path)
+        self.modelo = QDirModel()
+        self.treeViewBuscador.setModel(self.modelo)
+        self.treeViewBuscador.setRootIndex(self.modelo.index('{}/Documentos/'.format(path_ruta_local)))
+
+    def jalar_path(self, Qmodelidx):
+        print(self.modelo.filePath(Qmodelidx))
 
     def abrir_pdf(self):
         path, _ = QFileDialog.getOpenFileName(self, "Seleccione un Archivo", "", "pdf(*.pdf)")
@@ -29,6 +60,20 @@ class VentanaPrincipal(QMainWindow):
         if not self.VisualizadorPdf.setControl("Adobe PDF Reader"):
             return QMessageBox.critical(self,"Error","No se pudo abrir el archivo Adobe PDF Reader")
         self.VisualizadorPdf.dynamicCall("LoadFile(const QString&)", path)
+
+    def go_to_super_inicio(self):
+        self.ir_super_inicio = VistaSuperInicio()
+        self.ir_super_inicio.__init__()
+
+        self.ir_super_inicio.show()
+
+    def go_to_admin_area(self, name):
+        self.empresa_id = db.session.query(User.empresa_id).filter(User.username == name).first()
+        self.nombre_empresa = db.session.query(Empresa.nombre_empresa).filter(Empresa.id == self.empresa_id[0]).first()
+        texto = str(self.nombre_empresa[0])
+        self.ir_super_inicio = VistaAdminInicio(texto)
+
+        self.ir_super_inicio.show()
 
 def main():
     print('Todo tranqui')
